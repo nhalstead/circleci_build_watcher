f4a312335efd048fc2e43a2721d29a4d568c1f14
Noah Halstead <nhalstead00@gmail.com>
2020-04-02 00:08:38 -0400

    Added React and Electron Build.
    I've added a basic interface of what I am looking to make to be displayed.
    
    I've used SVGs for the Icons and left the source where applicable for the icons in use.

---
+++ public/electron.js
@@ -0,0 +1,212 @@
+const {app, BrowserWindow, Menu, Tray, nativeImage, dialog, screen, shell} = require('electron');
+const {autoUpdater} = require("electron-updater");
+const isDev = require('electron-is-dev');
+let mainWindow, tray;
+
+// @link https://getstream.io/blog/takeaways-on-building-a-react-based-app-with-electron/
+
+/*
+ * Only allow once instance to be open at a time
+ */
+if (!app.requestSingleInstanceLock()) {
+	app.quit();
+}
+
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+	// A new instance was attempted to be opened.
+	showApp();
+});
+
+/*
+ * App entry point & auto update
+ */
+app.on('ready', () => {
+	app.setAppUserModelId('nhalstead.circleciWatcher');
+	autoUpdater.checkForUpdates();
+});
+
+autoUpdater.on('update-available', () => {
+	dialog.showMessageBox({
+		type: 'info',
+		title: 'Found Updates',
+		message: 'Found updates, app installing updates and restarting automatically'
+	});
+});
+
+autoUpdater.on('update-downloaded', () => {
+	autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('update-not-available', () => {
+	app_launch();
+});
+
+autoUpdater.on('error', () => {
+	app_launch();
+});
+
+/*
+ * App Window Creation Methods
+ */
+function app_launch() {
+	createWindow();
+	createTaskTray();
+	createMenu();
+}
+
+function createWindow() {
+	let primaryDisplay = screen.getPrimaryDisplay();
+	const height = 300;
+	const width = 300;
+	const gap = 32;
+
+	mainWindow = new BrowserWindow({
+		height,
+		width,
+		y: (primaryDisplay.size.height - height) - gap,
+		x: (primaryDisplay.size.width - width) - gap,
+		icon: 'src/assets/icon.png',
+		show: false,
+		frame: false,
+		alwaysOnTop: true
+	});
+
+	mainWindow.setMenu(null);
+	mainWindow.setTitle('CircleCI Build Watcher');
+
+	mainWindow.loadURL(
+		isDev
+			? 'http://localhost:3933' // 'https://nhalstead.me/circle.test.html'
+			: `file://${path.join(__dirname, '../build/index.html')}`,
+	);
+
+	if (isDev) {
+		const {
+			default: installExtension,
+			REACT_DEVELOPER_TOOLS,
+			REDUX_DEVTOOLS,
+		} = require('electron-devtools-installer');
+
+		installExtension(REACT_DEVELOPER_TOOLS)
+			.then(name => {
+				console.log(`Added Extension: ${name}`);
+			})
+			.catch(err => {
+				console.log('An error occurred: ', err);
+			});
+
+		installExtension(REDUX_DEVTOOLS)
+			.then(name => {
+				console.log(`Added Extension: ${name}`);
+			})
+			.catch(err => {
+				console.log('An error occurred: ', err);
+			});
+	}
+
+	mainWindow.on('close', function (event) {
+		if (process.platform === 'win32') {
+			event.preventDefault();
+			mainWindow.hide();
+		}
+	});
+
+	mainWindow.webContents.once('did-finish-load', () => {
+		mainWindow.show();
+	});
+
+	// Force the new windows to open in the default browser.
+	mainWindow.webContents.on('new-window', function(event, url){
+		event.preventDefault();
+		shell.openExternal(url);
+	});
+}
+
+
+/*
+ * Tray specific functions
+ */
+function createTaskTray() {
+	try {
+		const trayIcon = nativeImage.createFromPath('favicon.ico');
+		tray = new Tray(trayIcon);
+
+		tray.setToolTip('CircleCI Build Watcher');
+		tray.on('click', showApp);
+		tray.on('double-click', showApp);
+
+		const contextMenu = Menu.buildFromTemplate([
+			{label: 'Reload App', click: reloadApp},
+			{type: 'separator'},
+			{label: 'Show', click: showApp},
+			{label: 'Exit', click: exitApp}
+		]);
+		tray.setContextMenu(contextMenu);
+	} catch (e) {
+		// Failed to create Tray Icon
+		console.log('Failed to Create Icon', e);
+	}
+}
+
+function createMenu () {
+	const template = [
+		{
+			label: 'File',
+			submenu: [{ role: 'about' }, { role: 'quit' }],
+		},
+		{
+			label: 'View',
+			submenu: [
+				{ role: 'reload' },
+				{ role: 'forcereload' },
+				{ role: 'toggledevtools' },
+				{ type: 'separator' },
+				{ role: 'resetzoom' },
+				{ role: 'zoomin' },
+				{ role: 'zoomout' },
+				{ type: 'separator' },
+				{ role: 'togglefullscreen' },
+			],
+		},
+		{
+			role: 'window',
+			submenu: [{ role: 'minimize' }, { role: 'close' }],
+		},
+		{
+			role: 'help',
+			submenu: [
+				{
+					click() {
+						shell.openExternal(
+							'https://github.com/nhalstead',
+						);
+					},
+					label: 'File Issue on GitHub',
+				},
+			],
+		},
+	];
+
+	Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+};
+
+function reloadApp() {
+	mainWindow.webContents.reload();
+}
+
+function showApp() {
+	mainWindow.show();
+	if (mainWindow) {
+		if (mainWindow.isMinimized()) mainWindow.restore();
+		mainWindow.focus();
+	}
+}
+
+function exitApp() {
+	mainWindow.close();
+	app.exit();
+}
+
+app.on('activate', function () {
+	if (mainWindow === null) createWindow()
+});
---
+++ src/components/BuildCard.js
@@ -0,0 +1,70 @@
+import React, {Component} from "react";
+import failedIcon from './failed.svg';
+import succeededIcon from './succeeded.svg';
+import runningIcon from './running.svg';
+import waitingIcon from './waiting.svg';
+import unknownIcon from './unknown.svg';
+
+class BuildCard extends Component {
+
+	render() {
+		const {status, org, repo, buildNumber, author, authorIcon} = this.props;
+		let statusCss = "unknown";
+		let statusText = "Unknown";
+		let statusIcon = unknownIcon;
+
+		if(status === "succeeded") {
+			statusCss = "succeeded";
+			statusText = "Success";
+			statusIcon = succeededIcon;
+		}
+		else if(status === "failed") {
+			statusCss = "failed";
+			statusText = "Failed";
+			statusIcon = failedIcon;
+		}
+		else if(status === "canceled") {
+			statusCss = "canceled";
+			statusText = "Canceled";
+			statusIcon = failedIcon;
+		}
+		else if(status === "running") {
+			statusCss = "running";
+			statusText = "Running";
+			statusIcon = runningIcon;
+		}
+		else if(status === "waiting") {
+			statusCss = "waiting";
+			statusText = "Queued";
+			statusIcon = waitingIcon;
+		}
+
+		return (
+			<div className="build_card">
+				<p className="build_name">
+					{org} / {repo}
+				</p>
+
+				<div className="status-area" style={{paddingTop: "8px"}}>
+					<div className="user-icon" data-component="frontend.components.pieces.status/badge">
+						<div className="status-icon">
+							<img src={authorIcon || unknownIcon} className="dashboard-icon" />
+						</div>
+						<div className="badge-label">{ author || <i>unknown</i>}</div>
+					</div>
+					<a href="https://circleci.com/gh/securecoders/sd-api/313" target="_blank" style={{display: "inline-block"}}>
+						<div title={statusText} className={statusCss} data-component="frontend.components.pieces.status/badge">
+							<div className="status-icon" style={{paddingTop: "4px"}}>
+								<img src={statusIcon} className="css-1rozygh"/>
+							</div>
+							<div className="badge-label">{ statusText } #{ buildNumber }</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		)
+	}
+
+}
+
+export default BuildCard;
