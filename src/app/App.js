95a58c8b8bf912dc845d3cb246d44174cd10571a
Noah Halstead <nhalstead00@gmail.com>
2020-04-25 00:25:21 -0400

    Adjusted the way the window works for doing a window drag.
    Now at the bottom center you have a grab point to move it, grip lines.

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,7 @@ import BuildCard from "./components/BuildCard";
 import './App.css';
 import {getEventsFromConfig} from "../store/actions/circle_ci";
 import EmptyCard from "./components/EmptyCard";
+import gripLines from "./grip-lines.svg";
 
 class App extends Component {
 
@@ -21,38 +22,54 @@ class App extends Component {
 		const {events} = this.props;
 
 		return (
-			<div id="content">
-				{events.length !== 0 ? (
-					events.map(event => {
-						let url = `https://app.circleci.com/pipelines/${event.type}/${event.organization}/${event.repository}/${event.number}/workflows/${event.workflow}/jobs/${event.number}`;
-						if(!event.workflow) {
-							// For Orgs who don't have Pipeline Processing Enabled
-							url = `https://app.circleci.com/pipelines/${event.type}/${event.organization}/${event.repository}/jobs/${event.number}`;
-						}
+			<React.Fragment>
+				<div style={{
+					position: "fixed",
+					bottom: 0,
+					display: "flex",
+					justifyContent: "center",
+					width: "95vw",
+				}}>
+					<div style={{
+						WebkitAppRegion: "drag",
+						marginBottom: "4px"
+					}}>
+						<img src={gripLines} alt={"grab window mark"} className={"grab"}/>
+					</div>
+				</div>
+				<div id="content">
+					{events.length !== 0 ? (
+						events.map(event => {
+							let url = `https://app.circleci.com/pipelines/${event.type}/${event.organization}/${event.repository}/${event.number}/workflows/${event.workflow}/jobs/${event.number}`;
+							if(!event.workflow) {
+								// For Orgs who don't have Pipeline Processing Enabled
+								url = `https://app.circleci.com/pipelines/${event.type}/${event.organization}/${event.repository}/jobs/${event.number}`;
+							}
 
-						return (
-							<BuildCard
-								key={`card-${event.organization}-${event.repository}-${event.number}`}
-								status={event.status}
-								org={event.organization}
-								repo={event.repository}
-								branch={event.branch}
-								buildNumber={event.number}
-								author={event.username}
-								authorIcon={event.logo}
-								timestamp={event.timestamp}
-								isPullRequest={event.isPullRequests}
-								pullRequest={event.pullRequest}
-								link={url}
-							/>
-						)
-					})
-				) : (
-					<EmptyCard
-						message={"No builds to show in the last 4 hours."}
-					/>
-				)}
-			</div>
+							return (
+								<BuildCard
+									key={`card-${event.organization}-${event.repository}-${event.number}`}
+									status={event.status}
+									org={event.organization}
+									repo={event.repository}
+									branch={event.branch}
+									buildNumber={event.number}
+									author={event.username}
+									authorIcon={event.logo}
+									timestamp={event.timestamp}
+									isPullRequest={event.isPullRequests}
+									pullRequest={event.pullRequest}
+									link={url}
+								/>
+							)
+						})
+					) : (
+						<EmptyCard
+							message={"No builds to show in the last 4 hours."}
+						/>
+					)}
+				</div>
+			</React.Fragment>
 		);
 	}
 }
